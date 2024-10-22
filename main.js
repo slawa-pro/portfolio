df81af7bf1493cb14aece10a363dcd32da51954c
Vyacheslav <va.povhleb@gmail.com>
2024-10-22 19:00:36 +0300

    refactor: post/GET(Mon, Thu 22, 2024 19:00:04)

--- main.js
+++ main.js
@@ -11,3 +11,75 @@ burgerMenu.addEventListener('click', () => {
   burgerMenu.classList.toggle('change');
   mobileBurgerMenu.classList.toggle('show');
 });
+
+//btn-header 
+const modal = document.getElementById("modal");
+const btn = document.querySelector(".header-btn");
+const closeBtn = document.querySelector(".close");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+//form
+const botToken = 'AAG4Q8wq1dYY3jW0iXXne8AG-U0Wn_OfoZ0';
+const chatId = 'https://t.me/+kCEWC_3LhbAxYjAy'; 
+
+const telegramAPI = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+
+async function sendEmailTelegram(event){
+  event.preventDefault(); //отменим перезагрузку (SPA)
+
+  const form = event.target;
+  const formBtn = document.querySelector('.submit-btn');
+
+  const { email, text } = Object.fromEntries(new FormData(form).entries());// превр. данные формы в массив
+
+  console.log('Email:', email);
+  console.log('Message:', text);
+  
+
+    const message = `
+    Новый запрос с сайта:
+    Обратный адрес: ${email}
+    Сообщение: ${text}
+  `;
+
+  try {
+    const response = await fetch(telegramAPI, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        message: text
+      }) 
+    });
+
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      console.log('Success:', jsonResponse);
+      alert('Your request has been passed');
+    } else {
+      console.error('Error from server:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Network error:', error);
+  }
+
+
+
+
+}
